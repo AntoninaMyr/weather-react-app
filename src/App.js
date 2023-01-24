737e203f4bf5f1bbe565d3e65a9ef56ebeea4a96
AntoninaMyr <antoninabard@gmail.com>
2023-01-23 22:41:06 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
@@ -8,13 +8,15 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by{" "}
-        <a href="https://heartfelt-naiad-6b0a98.netlify.app/" 
-        target="_blank" rel="noopener">
+        <a href="https://heartfelt-naiad-6b0a98.netlify.app/"
+        // eslint-disable-next-line 
+        target="_blank">
         Antonina Myronenko</a>
         {" "} and is {" "}
         <a 
           href="https://github.com/AntoninaMyr/weather-react-app"
-          target="_blank" rel="noopener">
+          // eslint-disable-next-line 
+          target="_blank">
             open-sourced on GitHub
         </a>
       </footer>
