2af55a1ea356de512dcc8e2215b80f97a033b604
AntoninaMyr <antoninabard@gmail.com>
2023-01-23 22:25:21 -0600

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import React from 'react';
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;    
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://heartfelt-naiad-6b0a98.netlify.app/" 
+        target="_blank" rel="noopener">
+        Antonina Myronenko</a>
+        {" "} and is {" "}
+        <a 
+          href="https://github.com/AntoninaMyr/weather-react-app"
+          target="_blank" rel="noopener">
+            open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
