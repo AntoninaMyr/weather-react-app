294ce7b898d2aab87c9e25fc66e8707a4a7b87f1
AntoninaMyr <antoninabard@gmail.com>
2023-02-02 22:56:01 -0600

    Built structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from "react";
-
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-        <div className="conteiner">
-          <h1>Weather App</h1>
+        <div className="container">
+          <Weather />
           <footer>
             This project was coded by{" "}
             <a href="https://heartfelt-naiad-6b0a98.netlify.app/"
-            target="blank">
+            target="blank"
+            rel="noopener noreferrer">
             Antonina Myronenko</a>
             {" "} and is {" "}
             <a 
               href="https://github.com/AntoninaMyr/weather-react-app"
-              target="blank">
+              target="blank"
+              rel="noopener noreferrer">
                 open-sourced on GitHub
-            </a>
+            </a> {" "}
+            and{" "}
+          <a
+            href="https://reliable-tartufo-c2c3d7.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+
           </footer>
-      </div>
+        </div> 
     </div>
   );
 }
