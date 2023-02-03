a66ca564cb41111eab2d3a614e7ed3710149914b
AntoninaMyr <antoninabard@gmail.com>
2023-02-02 20:39:10 -0600

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,19 +5,21 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://heartfelt-naiad-6b0a98.netlify.app/"
-        target="blank">
-        Antonina Myronenko</a>
-        {" "} and is {" "}
-        <a 
-          href="https://github.com/AntoninaMyr/weather-react-app"
-          target="blank">
-            open-sourced on GitHub
-        </a>
-      </footer>
+        <div className="conteiner">
+          <h1>Weather App</h1>
+          <footer>
+            This project was coded by{" "}
+            <a href="https://heartfelt-naiad-6b0a98.netlify.app/"
+            target="blank">
+            Antonina Myronenko</a>
+            {" "} and is {" "}
+            <a 
+              href="https://github.com/AntoninaMyr/weather-react-app"
+              target="blank">
+                open-sourced on GitHub
+            </a>
+          </footer>
+      </div>
     </div>
   );
 }
